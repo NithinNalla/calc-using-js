8b43b9623cdd6e4135a0ee165f83a73743b74453
nithinnalla <97667378+NithinNalla@users.noreply.github.com>
2023-02-18 16:44:05 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,40 @@
+function sum()
+{
+    let x=document.getElementById('num1').value;
+    let y=document.getElementById('num2').value;
+    let a = parseInt(x);
+    let b=parseInt(y)
+    let k=a+b
+    document.getElementById("demo").innerHTML ="Result :-"+k;
+    console.log(k);
+}
+function sub()
+{
+    let x=document.getElementById('num1').value;
+    let y=document.getElementById('num2').value;
+    let a = parseInt(x);
+    let b=parseInt(y)
+    let k=a-b
+    document.getElementById("demo").innerHTML ="Result :-"+k;
+    console.log(k);   
+}
+function mul()
+{
+    let x=document.getElementById('num1').value;
+    let y=document.getElementById('num2').value;
+    let a = parseInt(x);
+    let b=parseInt(y)
+    let k=a*b
+    document.getElementById("demo").innerHTML ="Result :-"+k;
+    console.log(k);   
+}
+function div()
+{
+    let x=document.getElementById('num1').value;
+    let y=document.getElementById('num2').value;
+    let a = parseInt(x);
+    let b=parseInt(y)
+    let k=a/b
+    document.getElementById("demo").innerHTML ="Result :-"+k;
+    console.log(k);   
+}
